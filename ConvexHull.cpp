9ae75fbae906d772d03ef392c6a26daa1a5ed95b
Miquel <msimeonuni@gmail.com>
2025-06-22 21:35:34 +0200

    Add files via upload

--- ConvexHull.cpp
+++ ConvexHull.cpp
@@ -5,6 +5,7 @@ struct Point{
   long long x, y;
 };
 
+//Calcula el Área del Polígono.
 double PoligonArea(const vector<Point>& fig){
   double res = 0;
   for(unsigned i = 0;i<fig.size();i++){
@@ -15,34 +16,43 @@ double PoligonArea(const vector<Point>& fig){
   return fabs(res)/2;
 }
 
+//Calcula el determinante 2D (producto vectorial) para saber el ángulo que forman: izquierda (>0), derecha (<0) o colineal (= 0).
 double crossProduct(Point A, Point B, Point C){
   return A.x * B.y + B.x * C.y + C.x * A.y - B.y * C.x - C.y * A.x - A.y * B.x;
 }
 
+//Calcula la distancia al cuadrado entre dos puntos (para ordenar puntos colineales por cercanía al punto base).
 double squaredDistance(Point A, Point B){
   return (A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y);
 }
 
 vector<Point> convexHull(vector<Point> &points){
-  vector<Point> hull;
+
+  if(points.size() <= 1) return points;
+
   Point lowest = points[0];
+  //Primero, buscamos el punto más bajo y si hay un empate, el más a la izquierda.
   for(unsigned i = 1;i<points.size();i++){
     if(points[i].y < lowest.y || ((points[i].y == lowest.y) && (points[i].x < lowest.x))){
       lowest = points[i];
     }
   }
-
+  //Ordenamos los puntos de menor ángulo a mayor ángulo que forman respecto el punto base y si hay empate, se ordena por distancias.
   sort(points.begin(), points.end(), [&](const Point& A, const Point& B){
-    double cp = crossProduct(A,B,lowest);
+    double cp = crossProduct(lowest, A, B);
     if(cp == 0){
-      return squaredDistance(A,lowest) < squaredDistance(B, lowest);
+      return squaredDistance(lowest, A) < squaredDistance(lowest, B);
     }
     return cp > 0;
   });
-
+  
+  //Se van agregando los puntos.
+  vector<Point> hull;
+  if (points.size() < 3) return points;
   hull.push_back(points[0]);
   hull.push_back(points[1]);
   for(unsigned i = 2;i<points.size();i++){
+    //Si los 3 puntos hacen un giro a la derecha, se quita el del medio (hull[hull.size()-1]).
     while(hull.size() > 1 && crossProduct(hull[hull.size()-2], hull[hull.size()-1], points[i]) < 0){
       hull.pop_back();
     }
@@ -60,12 +70,12 @@ void solve(){
     cin >> x >> y;
     Points.emplace_back(Point{x,y});
   }
-  //sol = convexHull(Points);
-  cout << 2*PoligonArea(Points) << endl;
-  /*cout << sol.size() << endl;
+  sol = convexHull(Points);
+  //cout << 2*PoligonArea(Points) << endl;
+  cout << sol.size() << endl;
   for(Point A : sol){
     cout << A.x << " " << A.y << endl;
-  }*/
+  }
 }
  
  
