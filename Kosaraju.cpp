be6db0e9848c26c5fed50d69340c02b47092c1f8
Miquel <msimeonuni@gmail.com>
2025-07-01 17:23:40 +0200

    Add files via upload

--- Kosaraju.cpp
+++ Kosaraju.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 struct Nodo{
   int nodo, vecinos;
 };
 
-typedef long long ll;
+//Dfs recursivo que guarda el recorrido.
 void dfs(vector<vector<int>>& adj, int ini, vector<bool>& visited, vector<int>& res){
   for(int neighbour : adj[ini]){
     if(visited[neighbour]) continue;
@@ -21,33 +23,38 @@ void dfs(vector<vector<int>>& adj, int ini, vector<bool>& visited, vector<int>& 
 void solve() {
   int n;
   while(cin >> n){
-    vector<vector<int>> v(n);
+    //Guardamos las carreteras y sus inversas.
+    vector<vector<int>> aristas(n);
     vector<vector<int>> inverse(n);
+
     int house, carreteras;
     while(cin >> house){
+
       if(house == 0) break;
       house--;
+
       cin >> carreteras;
       for(int i = 0;i<carreteras;i++){
         int a;
         cin >> a;
         a--;
-        v[house].push_back(a);
+        aristas[house].push_back(a);
         inverse[a].push_back(house);
       }
     }
-    int res = 1;
+
+    int resultado = 1;
     vector<bool> visited1(n);
 
     vector<bool> visited2(n);
 
     vector<int> Recorrido;
-    //Primero haces un dfs normal guardando el recorrido.
+    
+    //Primero haces un dfs normal, desde todos los nodos, guardando el recorrido.
     for(int i = 0;i<n;i++){
       if(!visited1[i]){
         visited1[i] = true;
-        dfs(v, i, visited1, Recorrido);
-        //visited1[i] = true;
+        dfs(aristas, i, visited1, Recorrido);
       }
       
     }
@@ -61,12 +68,13 @@ void solve() {
 
       if(!visited2[i]){
         visited2[i] = true;
+        //En el dfs, hacemos uso de las aristas inversas.
         dfs(inverse, i, visited2, aux);
-        res = max(res,(int) aux.size());
-        //visited2[i] = true;
+        resultado = max(resultado, (int) aux.size());
       }
     }
-    cout << res << endl;
+    //El objetivo es encontrar el grupo ciclico cerrado más grande, es decir, la Componente Fuertemente Conexa con más elementos del grafo.
+    cout << resultado << endl;
   }
 }
 
